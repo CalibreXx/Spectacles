a4ecb75b7a87f29b75f5c5c9cf306907b1e4cf2f
CalibreXx <48781590+CalibreXx@users.noreply.github.com>
2020-08-26 10:44:06 +0800

    Update ESP32_EG_BINARY_STORAGE.ino

--- ESP32 Codes/ESP32_EG_BINARY_STORAGE/ESP32_EG_BINARY_STORAGE.ino
+++ ESP32 Codes/ESP32_EG_BINARY_STORAGE/ESP32_EG_BINARY_STORAGE.ino
@@ -543,29 +543,29 @@ void readFile(fs::FS &fs, const char * path) {
   struct splitLong LongByteConverter;
   int counter = 0;
   bool sendNow = false;
-  byte SDData_Byte[28] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  byte SDData_Byte[13] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
   while ( file.available()) {
     counter += 1;
     file.read((uint8_t *)&myData, sizeof(myData));
     FiveByteConverter.value = myData.epochTime_SD;
-    SDData_Byte[0 + sendNow * 14] = FiveByteConverter.split[4]; //EPOCH
-    SDData_Byte[1 + sendNow * 14] = FiveByteConverter.split[3];
-    SDData_Byte[2 + sendNow * 14] = FiveByteConverter.split[2];
-    SDData_Byte[3 + sendNow * 14] = FiveByteConverter.split[1];
-    SDData_Byte[4 + sendNow * 14] = FiveByteConverter.split[0];
-    SDData_Byte[5 + sendNow * 14] = myData.tof1_SD; //TOF
-    SDData_Byte[6 + sendNow * 14] = myData.tof2_SD;
-    SDData_Byte[7 + sendNow * 14] = myData.tof3_SD;
-    SDData_Byte[8 + sendNow * 14] = myData.accel_SD; // ACCEL
-    SDData_Byte[9 + sendNow * 14] = myData.pitch_SD;
-    SDData_Byte[10 + sendNow * 14] = myData.roll_SD;
+    SDData_Byte[0] = FiveByteConverter.split[4]; //EPOCH
+    SDData_Byte[1] = FiveByteConverter.split[3];
+    SDData_Byte[2] = FiveByteConverter.split[2];
+    SDData_Byte[3] = FiveByteConverter.split[1];
+    SDData_Byte[4] = FiveByteConverter.split[0];
+    SDData_Byte[5] = myData.tof1_SD; //TOF
+    SDData_Byte[6] = myData.tof2_SD;
+    SDData_Byte[7] = myData.tof3_SD;
+    SDData_Byte[8] = myData.accel_SD; // ACCEL
+    SDData_Byte[9] = myData.pitch_SD;
+    SDData_Byte[10] = myData.roll_SD;
     LongByteConverter.value =  myData.ldr_SD;
-    SDData_Byte[11 + sendNow * 14] = LongByteConverter.split[1]; //LDR
-    SDData_Byte[12 + sendNow * 14] = LongByteConverter.split[0];
+    SDData_Byte[11] = LongByteConverter.split[1]; //LDR
+    SDData_Byte[12] = LongByteConverter.split[0];
 
     if (deviceConnected && sendNow == true ) {
       delay(3);
-      DATA_SEND_Characteristic->setValue(SDData_Byte, 28); // 1 = 1 byte = 8 bits
+      DATA_SEND_Characteristic->setValue(SDData_Byte, 13); // 1 = 1 byte = 8 bits
       DATA_SEND_Characteristic->notify();
     }
     if (!deviceConnected) {
